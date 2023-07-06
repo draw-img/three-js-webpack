59644588ce637224cc27c6f1495cdd17ff4f95e2
Bae Ha-eun <dncl46@gmail.com>
2023-07-06 17:03:29 +0900

    기본구조(scene, camera, renderer), 배경색 바꾸기

--- src/index.js
+++ src/index.js
@@ -3,7 +3,23 @@ import { WEBGL } from './webgl'
 
 if (WEBGL.isWebGLAvailable()) {
   //코드작성
-  console.log(THREE)
+  //장면
+  const scene = new THREE.Scene()
+  //장면 색상
+  scene.background = new THREE.Color(0x004fff)
+
+  //카메라
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+  //렌더러
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  //어디에 어떤 코드에 출력할건지
+  document.body.appendChild(renderer.domElement);
+
+  renderer.render(scene, camera)
+
 } else {
   var warning = WEBGL.getWebGLErrorMessage()
   document.body.appendChild(warning)
